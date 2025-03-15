6d87c3e837f40fb798fe1fccf54181e43d732d80
Diogo Souza <Diogo_souza2019@hotmail.com>
2025-03-14 21:25:43 -0300

    menu principal v1

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import os
+from time import sleep
 
+
+def menu_principal():
+    os.system("cls")
+    print("🌐 1 - Python")
+    print("🖥️  2 - Hardware")
+    print("⚙️  3 - Software")
+    print("👮 4 - Cyber Segurança")
+    print("🎮 5 - Games")
+    print("📝 6 - Termos")
+    print("📝 7 - Sair")
+
+def python():
+    print("Abrindo Python!")
+    print("Python e feito para programadores")
+    #Voltar = int(input("Tecle 9 para sair "))
+    #if Voltar == 9:
+     #   menu_principal()
+    
+    
 
 print("Olá, bem-vindo à Plataforma Educacional")
 
@@ -9,7 +29,7 @@ login_correto = "admin"
 
 while True:
     login = input("Digite a login: ")
-    senha = input("Digite o senha: ") 
+    senha = input("Digite o senha: ")
     if login == login_correto and senha == senha_correta:
         print("Acesso permitido!")
         break
@@ -18,20 +38,13 @@ while True:
         os.system("cls")
     
 
-
-os.system("clear")
-def menu_principal():
-    print("1 - Python")
-    print("2 - Hardware")
-    print("3 - Software")
-    print("4 - Cyber Segurança")
-    print("5 - Games")
-    print("6 - Termos")
-
 while True:
     menu_principal()
-    escolha = input("Digite o numero que representa sua escolha")
-    if escolha == 7:
+    escolha = int(input("Digite o numero que representa sua escolha "))
+    if escolha == 1:
+        python()
+        sleep(10)
+    elif escolha == 7:
         print("Saindo")
-        break
+        exit()
 
